b81518da2fc4c7d8b0285205853ecefeb3fbb2c5
Denis Zlobin <zlobin.d.al@gmail.com>
2023-09-06 10:23:32 +0300

    Add script testing description
    Signed-off-by: Denis Zlobin <zlobin.d.al@gmail.com>

--- send_mail.sh
+++ send_mail.sh
@@ -5,10 +5,14 @@ mailBody="Test message"
 
 host="smtp.yandex.ru"
 port="587"
-mailFrom="<username>"
-user="<username>"
+mailFrom="<yandex_email>"
+user="<yandex_email>"
 password="<password>"
 mailTo="<destination_email>"
 
 echo -e "$mailSubject\n\n$mailBody" | \
-    msmtp --debug --auth="on" --tls="on" --tls-starttls="on" --tls-certcheck="off" --host=$host --port=$port --user=$user --passwordeval="echo $password" --from=$mailFrom $mailTo
+    msmtp --debug \
+          --auth="plain" \
+          --tls="on" --tls-starttls="on" --tls-certcheck="off" \
+          --host=$host --port=$port --user=$user --passwordeval="echo $password" \
+          --from=$mailFrom $mailTo
